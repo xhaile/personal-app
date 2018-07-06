eda02a00e4fa117520b3ca17c0666582e7b38f4b
niujizhan <niujizhan@baicheng.com>
2018-07-06 14:06:47 +0000

    路由文件

---
+++ app/route.php
@@ -0,0 +1,9 @@
+Route::group(array("domain"=>"apply.dev.baicheng.com"),function(){
+	Route::get("list", array("as"=>"user-list", "userContoller@list"));
+	Route::get("detail_{user_id}", array(
+		"as"=>"detail", 
+		"userController@detail"))->where(array(
+		"user_id"=>"[\d]+"
+	));
+
+});
