ccec130d88602425e36c10a45fe9db482051f59c
xhaile <15810865766@163.com>
2018-07-25 21:13:14 +0800

    add get host method

--- test.php
+++ test.php
@@ -1,5 +1,14 @@
 <?php
 
 class ArrayHelper{
+	public static function getHost()
+	{
+		$current_host = $_SERVER['HTTP_HOST'];
 
+		$url_arr = parse_url($current_host);
+		$host = current($url_host);
+
+		return $host;
+
+	}
 }
